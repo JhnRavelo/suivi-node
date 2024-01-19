99ab39013603f6c38590e7bb6b380924605e1730
JhnRavelo <johnravelo135@gmail.com>
2024-01-19 14:13:08 +0300

    middleware multer for upload image

--- routers/suivisRouter.js
+++ routers/suivisRouter.js
@@ -1,10 +1,36 @@
-const express = require("express")
-const verifyJWT = require("../middlewares/verifyJWT")
-const { getByProduct, addSuivi, deleteSuivi } = require("../controllers/suivisController")
-const router = express.Router()
+const express = require("express");
+const verifyJWT = require("../middlewares/verifyJWT");
+const {
+  getByProduct,
+  addSuivi,
+  deleteSuivi,
+  uploadImageSuivi,
+} = require("../controllers/suivisController");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
-router.post("/getByProduct", verifyJWT, getByProduct)
-router.post("/addSuivi", verifyJWT, addSuivi)
-router.post("/delete", verifyJWT, deleteSuivi)
+const imgPath = path.join(__dirname, "..", "public", "img");
 
-module.exports = router
+const stokage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, imgPath);
+  },
+  filename: (req, file, callback) => {
+    const date = new Date();
+    callback(null, `${file.originalname}-${date.getDay()}-${date.getTime()}.png`);
+  },
+});
+
+const upload = multer({
+  storage: stokage,
+});
+
+const multipleImage = upload.any();
+
+router.put("/upload", multipleImage, uploadImageSuivi);
+router.post("/getByProduct", verifyJWT, getByProduct);
+router.post("/addSuivi", verifyJWT, addSuivi);
+router.post("/delete", verifyJWT, deleteSuivi);
+
+module.exports = router;
