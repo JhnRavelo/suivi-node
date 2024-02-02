1046e00e11f720f0f7c8f55b0d16ee2284bd7cda
JhnRavelo <johnravelo135@gmail.com>
2024-02-02 15:01:18 +0300

    add userController login-web

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,73 +4,157 @@ const bcrypt = require("bcrypt");
 
 const login = async (req, res) => {
   const { email, password } = await req.body;
-
-  const userLogin = await users.findOne({
-    where: {
-      email: {
-        [Op.eq]: email,
+  try {
+    const userLogin = await users.findOne({
+      where: {
+        email: {
+          [Op.eq]: email,
+        },
       },
-    },
-  });
+    });
 
-  if (!userLogin) return res.json({ success: false });
+    if (!userLogin) return res.json({ success: false });
 
-  const match = await bcrypt.compare(password, userLogin.password);
+    const match = await bcrypt.compare(password, userLogin.password);
 
-  if (!match) return res.json({ success: false });
+    if (!match) return res.json({ success: false });
 
-  let id = userLogin.id;
-  let role = userLogin.role;
+    let id = userLogin.id;
+    let role = userLogin.role;
 
-  const accesToken = users.prototype.generateToken(id, role);
-  const refreshToken = users.prototype.generateRefreshToken(id);
+    const accesToken = users.prototype.generateToken(id, role);
+    const refreshToken = users.prototype.generateRefreshToken(id);
 
-  userLogin.refreshToken = refreshToken;
+    userLogin.refreshToken = refreshToken;
 
-  await userLogin.save();
+    await userLogin.save();
 
-  res.json({
-    success: true,
-    accesToken,
-    refreshToken,
-    role,
-    name: userLogin.name,
-    email: userLogin.email,
-  });
+    res.json({
+      success: true,
+      accesToken,
+      refreshToken,
+      role,
+      name: userLogin.name,
+      email: userLogin.email,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
 };
 
-const logout = async(req, res)=>{
-  const {refreshToken} = await req.body
+const logout = async (req, res) => {
+  const { refreshToken } = await req.body;
 
-  if(!refreshToken) return res.json({success: false})
+  if (!refreshToken) return res.json({ success: false });
 
   const userLogout = await users.findOne({
     where: {
-      refreshToken: refreshToken
+      refreshToken: refreshToken,
+    },
+  });
+
+  if (!userLogout) return res.json({ success: false });
+
+  userLogout.refreshToken = "";
+  await userLogout.save();
+
+  res.json({ success: true });
+};
+
+const userRead = async (req, res) => {
+  const id = await req.user;
+
+  if (!id) return res.json({ success: false });
+
+  const user = await users.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!user) return res.json({ success: false });
+
+  res.json({ success: true, name: user.name, email: user.email });
+};
+
+const userLoginWeb = async (req, res) => {
+  try {
+    const { email, password } = await req.body;
+
+    const cookie = req.cookies;
+    console.log("LOGIN")
+    if (!email || !password) return res.json({ success: false });
+
+    const userLogged = await users.findOne({
+      where: {
+        email: {
+          [Op.eq]: email,
+        },
+      },
+    });
+
+    if (!userLogged) return res.json({ success: false });
+
+    if (userLogged.role != process.env.PRIME)
+      return res.json({
+        success: false,
+        error: "Vous n'avez pas la permission",
+      });
+
+    const match = await bcrypt.compare(password, userLogged.password);
+
+    if (!match)
+      return res.json({ success: false, error: "Connexion Invalide" });
+
+    const id = userLogged.id;
+    const role = userLogged.role;
+
+    let newRefreshToken = users.prototype.generateRefreshToken(id);
+    const accessToken = users.prototype.generateToken(id, role);
+
+    if (cookie?.jwt_ea_suivi) {
+      const refreshToken = cookie.jwt_ea_suivi;
+      const userFound = await users.findOne({
+        where: {
+          refreshToken: refreshToken,
+        },
+      });
+
+      if (!userFound) {
+        newRefreshToken = "";
+      }
+
+      res.clearCookie("jwt_ea_suivi", {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+      });
     }
-  })
 
-  if(!userLogout) return res.json({success: false})
+    userLogged.refreshToken = newRefreshToken;
 
-  userLogout.refreshToken = ""
-  await userLogout.save()
+    await userLogged.save();
 
-  res.json({success: true})
+    res.cookie("jwt_ea_suivi", newRefreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
-}
+    res.json({
+      success: true,
+      accessToken,
+      role,
+      name: userLogged.name,
+      email: userLogged.email,
+    });
 
-const userRead = async(req, res)=>{
-const id = await req.user
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, error: "Problème de serveur" });
+  }
+};
 
-if(!id) return res.json({success: false})
-
-const user = await users.findOne({where: {
-  id: id
-}})
-
-if(!user) return res.json({success: false})
-
-res.json({success: true, name: user.name, email: user.email})
-}
-
-module.exports = { login, logout, userRead };
+module.exports = { login, logout, userRead, userLoginWeb };
