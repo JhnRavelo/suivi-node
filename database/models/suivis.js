3488ff436da7e7476d634dc6dfcb2d94c8fc5764
JhnRavelo <johnravelo135@gmail.com>
2024-02-06 16:56:24 +0300

    update models suivi

--- database/models/suivis.js
+++ database/models/suivis.js
@@ -1,34 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
-    const suivis = sequelize.define("suivis", {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      observation: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      problem: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      solution: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
+  const suivis = sequelize.define("suivis", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    observation: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    problem: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    solution: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+
+  suivis.associate = (models) => {
+    suivis.belongsTo(models.products, {
+      onDelete: "CASCADE",
+      foreignKey: "productId",
     });
-  
-    suivis.associate = (models) => {
-      suivis.belongsTo(models.products, {
-        onDelete: "CASCADE",
-        foreignKey: "productId",
-      });
-      suivis.belongsTo(models.users, {
-        foreignKey: "userId",
-      });
-    };
-  
-    return suivis;
+    suivis.belongsTo(models.users, {
+      foreignKey: "userId",
+    });
+    suivis.hasMany(models.logs, {
+      onDelete: "CASCADE",
+      foreignKey: "suiviId",
+    });
   };
+
+  return suivis;
+};
