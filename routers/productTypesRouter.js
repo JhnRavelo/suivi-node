bad6589b4be5dd68a638e0117785752457818502
JhnRavelo <johnravelo135@gmail.com>
2024-02-08 15:41:12 +0300

    add route delete ProductTypeRouter

--- routers/productTypesRouter.js
+++ routers/productTypesRouter.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const verifyJWT = require("../middlewares/verifyJWT")
-const { getAllProductTypes, addProductType } = require("../controllers/productTypeController")
+const { getAllProductTypes, addProductType, deleteProductType } = require("../controllers/productTypeController")
 const router = express.Router()
 const multer = require("multer")
 
@@ -8,5 +8,6 @@ const memoryStokage = multer({storage:multer.memoryStorage()})
 
 router.get("/getAll", verifyJWT,getAllProductTypes)
 router.post("/add", verifyJWT, memoryStokage.any(), addProductType)
+router.delete("/:id", verifyJWT, deleteProductType)
 
 module.exports = router
