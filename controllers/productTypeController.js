7c06f38941b4479d57ef1ebd4616af7a793b25e4
JhnRavelo <johnravelo135@gmail.com>
2024-02-09 11:05:19 +0300

    update addProductType and updateProductType controller

--- controllers/productTypeController.js
+++ controllers/productTypeController.js
@@ -3,13 +3,7 @@ const fs = require("fs");
 const pdfPoppler = require("pdf-poppler");
 const path = require("path");
 
-const pdfPath = path.join(
-  __dirname,
-  "..",
-  "public",
-  "pdf",
-  "temporaire.pdf"
-);
+const pdfFolderPath = path.join(__dirname, "..", "public", "pdf");
 
 const pdfOutput = path.join(__dirname, "..", "public", "img");
 
@@ -25,70 +19,80 @@ const addProductType = async (req, res) => {
   try {
     const { name } = await req.body;
     const pdfBuffer = req.files[0]?.buffer;
-    let imagePaths = [];
-    let imageFiles = [];
-    let newPDF;
+    // let imagePaths = [];
+    // let imageFiles = [];
+    // let newPDF;
     if (!name) return res.json({ success: false });
     if (pdfBuffer) {
-      fs.writeFileSync(pdfPath, pdfBuffer);
       const originalname = req.files[0].originalname.split(".")[0];
       const date = new Date();
       const prefix = `${originalname}-${date.getDate()}-${
         date.getMonth() + 1
-      }-${date.getFullYear()}`;
-      const options = {
-        format: "jpg",
-        out_dir: pdfOutput,
-        out_prefix: prefix,
-        page: null,
-      };
+      }-${date.getFullYear()}.pdf`;
+      const pdfPath = path.join(pdfFolderPath, prefix);
+      fs.writeFileSync(pdfPath, pdfBuffer);
+      const newPDF = `${process.env.SERVER_PATH}/pdf/${prefix}`;
+      const newProductType = await productTypes.create({
+        name: name,
+        pdf: newPDF,
+      });
+      if (!newProductType) return res.json({ success: false });
+      const allProductTypes = await productTypes.findAll();
+      if (!allProductTypes) return res.json({ success: false });
+      res.json({ success: true, types: allProductTypes });
+      // const options = {
+      //   format: "jpg",
+      //   out_dir: pdfOutput,
+      //   out_prefix: prefix,
+      //   page: null,
+      // };
 
-      pdfPoppler
-        .convert(pdfPath, options)
-        .then(() => {
-          fs.unlinkSync(pdfPath);
-          fs.readdir(options.out_dir, async (err, files) => {
-            if (err) {
-              console.error(
-                "Erreur lors de la lecture du dossier de sortie :",
-                err
-              );
-              return;
-            }
+      // pdfPoppler
+      //   .convert(pdfPath, options)
+      //   .then(() => {
+      //     fs.unlinkSync(pdfPath);
+      //     fs.readdir(options.out_dir, async (err, files) => {
+      //       if (err) {
+      //         console.error(
+      //           "Erreur lors de la lecture du dossier de sortie :",
+      //           err
+      //         );
+      //         return;
+      //       }
 
-            imageFiles = files.filter((file) =>
-              file.startsWith(options.out_prefix)
-            );
-            if (imageFiles.length > 0) {
-              const response = imageFiles.map((item) =>
-                imagePaths.push(
-                  `${process.env.SERVER_PATH}/img/${item}`
-                )
-              );
-              await Promise.all(response);
+      //       imageFiles = files.filter((file) =>
+      //         file.startsWith(options.out_prefix)
+      //       );
+      //       if (imageFiles.length > 0) {
+      //         const response = imageFiles.map((item) =>
+      //           imagePaths.push(
+      //             `${process.env.SERVER_PATH}/img/${item}`
+      //           )
+      //         );
+      //         await Promise.all(response);
 
-              newPDF = imagePaths.join(",");
-              console.log(newPDF);
-              const newProductType = await productTypes.create({
-                name: name,
-                pdf: newPDF,
-              });
+      //         newPDF = imagePaths.join(",");
+      //         console.log(newPDF);
+      //         const newProductType = await productTypes.create({
+      //           name: name,
+      //           pdf: newPDF,
+      //         });
 
-              if (!newProductType)
-                return res.json({ success: false });
-              const allProductTypes = await productTypes.findAll();
+      //         if (!newProductType)
+      //           return res.json({ success: false });
+      //         const allProductTypes = await productTypes.findAll();
 
-              if (!allProductTypes)
-                return res.json({ success: false });
+      //         if (!allProductTypes)
+      //           return res.json({ success: false });
 
-              res.json({ success: true, types: allProductTypes });
-            }
-          });
-        })
-        .catch((error) => {
-          console.error("Erreur lors de la conversion :", error);
-          fs.unlinkSync(pdfPath);
-        });
+      //         res.json({ success: true, types: allProductTypes });
+      //       }
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     console.error("Erreur lors de la conversion :", error);
+      //     fs.unlinkSync(pdfPath);
+      //   });
     } else {
       const newProductType = await productTypes.create({
         name: name,
@@ -119,17 +123,23 @@ const deleteProductType = async (req, res) => {
     });
 
     if (deletedProductType.dataValues?.pdf) {
-      const arrayProductTypes =
-        deletedProductType.dataValues.pdf.split(",");
-      arrayProductTypes.map((item) => {
-        let name = item.split("/")[item.split("/").length - 1];
-        let deltePath = path.join(pdfOutput, name);
-        fs.unlinkSync(deltePath, (err) => {
-          if (err) {
-            console.log(err);
-          }
-        });
+      const pdfFile = deletedProductType.dataValues.pdf;
+      fs.unlinkSync(pdfFile, (err) => {
+        if (err) {
+          console.log(err);
+        }
       });
+      //   const arrayProductTypes =
+      //     deletedProductType.dataValues.pdf.split(",");
+      //   arrayProductTypes.map((item) => {
+      //     let name = item.split("/")[item.split("/").length - 1];
+      //     let deltePath = path.join(pdfOutput, name);
+      //     fs.unlinkSync(deltePath, (err) => {
+      //       if (err) {
+      //         console.log(err);
+      //       }
+      //     });
+      //   });
     }
 
     const result = await deletedProductType.destroy();
