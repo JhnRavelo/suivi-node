72d186a99cbcd56ed4c45e4bc30087532ae34203
JhnRavelo <johnravelo135@gmail.com>
2024-02-13 13:58:28 +0300

    update productsController addProduct and getAllProducts

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,27 +1,64 @@
-const { products } = require("../database/models");
+const { products, productTypes, users } = require("../database/models");
+const getProducts = require("../utils/getProducts");
 
 const addProduct = async (req, res) => {
-  const { type, location, devis, detail, dimension, client, chantier } = await req.body;
+  try {
+    const { type, location, devis, detail, dimension, client, chantier, tech } =
+      await req.body;
+    let productAdded;
+    const idUser = req.user;
 
-  const id = req.user;
+    if (!type || !location || !devis || !dimension)
+      return res.json({ success: false });
+    if (tech) {
+      productAdded = await products.create({
+        productTypeId: type,
+        location,
+        detail,
+        devis,
+        dimension,
+        userProductId: tech,
+        client,
+        chantier,
+      });
 
-  if (!type || !location || !devis || !dimension)
-    return res.json({ success: false });
+      if (!productAdded) return res.json({ success: false });
 
-  const productAdded = await products.create({
-    productTypeId: type,
-    location,
-    detail,
-    devis,
-    dimension,
-    userProductId: id,
-    client,
-    chantier
-  });
+      const filterProducts = await getProducts(products, productTypes, users, res);
+      console.log(filterProducts)
 
-  if (!productAdded) return res.json({ success: false });
+      res.json({ success: true, products: filterProducts });
+    } else {
+      productAdded = await products.create({
+        productTypeId: type,
+        location,
+        detail,
+        devis,
+        dimension,
+        userProductId: idUser,
+        client,
+        chantier,
+      });
 
-  res.json({ success: true, product: productAdded.id });
+      if (!productAdded) return res.json({ success: false });
+
+      res.json({ success: true, product: productAdded.id });
+    }
+  } catch (error) {
+    res.json({ success: false });
+    console.log(error);
+  }
 };
 
-module.exports = { addProduct };
+const getAllProducts = async (req, res) => {
+  try {
+    const filterProducts = await getProducts(products, productTypes, users, res);
+
+    res.json({ success: true, products: filterProducts });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
+};
+
+module.exports = { addProduct, getAllProducts };
