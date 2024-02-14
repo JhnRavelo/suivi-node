fdecc046f4ba53ed87096cf3d945a4f8984ef8d6
JhnRavelo <johnravelo135@gmail.com>
2024-02-14 16:29:45 +0300

    add read route to logRouter

--- routers/logsRouter.js
+++ routers/logsRouter.js
@@ -2,7 +2,9 @@ const express = require("express")
 const verifyJWT = require("../middlewares/verifyJWT")
 const { getLogs } = require("../controllers/logsController")
 const router = express.Router()
+const verifyRole = require("../middlewares/verifyRole")
 
 router.get("/", verifyJWT, getLogs)
+router.get("/read", verifyJWT, verifyRole(process.env.PRIME), getLogs)
 
 module.exports = router
