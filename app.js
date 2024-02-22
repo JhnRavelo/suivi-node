b9b99afc46e57900c5166e369573922f6c44fb31
JhnRavelo <johnravelo135@gmail.com>
2024-02-22 13:29:57 +0300

    add route to problem in app.js

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ db.sequelize.sync({ alter: true }).then(() => {
   });
 });
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  if (req.method === 'OPTIONS') {
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, X-Requested-With, Range"
+  );
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   } else {
     return next();
@@ -50,3 +53,5 @@ const productsRouter = require("./routers/productsRouter");
 app.use("/product", productsRouter);
 const logsRouter = require("./routers/logsRouter");
 app.use("/log", logsRouter);
+const problemsRouter = require("./routers/problemsRouter");
+app.use("/problem", problemsRouter);
