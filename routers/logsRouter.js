e0e319a718e3794de61499980a43ca5dd1c6c052
JhnRavelo <johnravelo135@gmail.com>
2024-03-13 10:55:09 +0300

    update logsRouter

--- routers/logsRouter.js
+++ routers/logsRouter.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const verifyJWT = require("../middlewares/verifyJWT")
-const { getLogs } = require("../controllers/logsController")
-const router = express.Router()
-const verifyRole = require("../middlewares/verifyRole")
+const express = require("express");
+const verifyJWT = require("../middlewares/verifyJWT");
+const { getLogs, readLogs } = require("../controllers/logsController");
+const router = express.Router();
+const verifyRole = require("../middlewares/verifyRole");
 
-router.get("/", verifyJWT, getLogs)
-router.get("/read", verifyJWT, verifyRole(process.env.PRIME), getLogs)
+router.get("/", verifyJWT, getLogs);
+router.get("/readLog", verifyJWT, verifyRole(process.env.PRIME), readLogs);
 
-module.exports = router
+module.exports = router;
