0593bcc4e23d160f221619ff1cdc63d00b351889
JhnRavelo <johnravelo135@gmail.com>
2024-03-13 08:56:50 +0300

    add createImage deleteImage getSuivisByProduct to suivisController

--- controllers/suivisController.js
+++ controllers/suivisController.js
@@ -11,6 +11,9 @@ const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 const getSuivis = require("../utils/getSuivis");
+const deleteImage = require("../utils/deleteImage");
+const createImage = require("../utils/createImage");
+const getSuivisByProduct = require("../utils/getSuivisByProduct");
 
 const imgPath = path.join(__dirname, "..", "public", "img");
 
@@ -30,14 +33,7 @@ const getByProduct = async (req, res) => {
 
     if (!isEmail) return res.json({ success: false });
 
-    const allSuivi = await suivis.findAll({
-      where: {
-        productId: id,
-      },
-      include: [{ model: users, attribute: ["id", "name"] }],
-    });
-
-    if (!allSuivi) return res.json({ success: false });
+    const allSuivi = await getSuivisByProduct(suivis, users, res, id, problems);
 
     const product = await products.findOne({
       where: {
@@ -77,154 +73,118 @@ const getByProduct = async (req, res) => {
       problems: filterProblems,
     });
   } catch (error) {
-    console.log(error);
+    res.json({ success: false });
+    console.log("ERROR getByProduct", error);
   }
 };
 
 const addSuivi = async (req, res) => {
-  const { productId, problem, solution, observation, problemId } = await req.body;
-  const userId = await req.user;
+  try {
+    const { productId, problem, solution, observation, problemId } =
+      await req.body;
+    const userId = await req.user;
 
-  if (!productId || !problem || !solution || !userId)
-    return res.json({ success: false });
+    if (!productId || !problem || !solution || !userId)
+      return res.json({ success: false });
+    const newSuivi = await suivis.create({
+      productId,
+      problem,
+      solution,
+      observation: `${observation ? observation : ""};`,
+      userId,
+    });
 
-  const newSuivi = await suivis.create({
-    productId,
-    problem,
-    solution,
-    observation: `${observation ? observation : ""};`,
-    userId,
-  });
+    if (!newSuivi) return res.json({ success: false });
 
-  if (!newSuivi) return res.json({ success: false });
-  
-  if (problemId) newSuivi.problemId = problemId
-
-  await newSuivi.save()
-
-  await logs.create({ suiviId: newSuivi.dataValues.id });
-
-  const allSuivis = await suivis.findAll({
-    where: {
-      productId: productId,
-    },
-    include: [{ model: users, attribute: ["id", "name"] }],
-  });
-
-  res.json({
-    success: true,
-    id: newSuivi.dataValues.id,
-    suivis: allSuivis,
-  });
+    if (problemId) {
+      newSuivi.problemId = problemId;
+      await newSuivi.save();
+    }
+    await logs.create({ suiviId: newSuivi.dataValues.id });
+    await uploadImageSuivi(req, res, newSuivi, productId);
+  } catch (error) {
+    res.json({ success: false });
+    console.log("ERROR addSuivi", error);
+  }
 };
 
 const deleteSuivi = async (req, res) => {
   let galleryArray;
   const { deleteId, productId } = await req.body;
-
-  if (!deleteId) return res.json({ success: false });
-
-  const deleteSuivi = await suivis.findOne({
-    where: {
-      id: deleteId,
-    },
-  });
-
-  if (!deleteSuivi) return res.json({ success: false });
-
-  const galleryString = deleteSuivi.dataValues.observation.split(";")[1];
-
-  if (galleryString && galleryString != "null") {
-    galleryArray = galleryString.split(",");
-  }
-
-  if (galleryArray) {
-    galleryArray.map((gallery) => {
-      let name = gallery.split("/")[gallery.split("/").length - 1];
-      let pathFile = path.join(imgPath, name);
-      fs.unlinkSync(pathFile, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    });
-  } else if (!galleryArray && galleryString && galleryString != "null") {
-    let name = galleryString.split("/")[galleryString.split("/").length - 1];
-    let pathFile = path.join(imgPath, name);
-    fs.unlinkSync(pathFile, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-
-  const result = await deleteSuivi.destroy();
-
-  if (!result) return res.json({ success: false });
-  if (productId) {
-    const allSuivis = await suivis.findAll({
+  try {
+    if (!deleteId) return res.json({ success: false });
+    const deleteSuivi = await suivis.findOne({
       where: {
-        productId: productId,
+        id: deleteId,
       },
-      include: [{ model: users, attribute: ["id", "name"] }],
     });
-    res.json({ success: true, suivis: allSuivis });
-  } else {
-    const filterSuivis = await getSuivis(suivis, users, products, productTypes);
-    res.json({ success: true, suivis: filterSuivis });
+
+    if (!deleteSuivi) return res.json({ success: false });
+    const galleryString = deleteSuivi.dataValues.observation.split(";")[1];
+
+    if (galleryString && galleryString != "") {
+      galleryArray = galleryString.split(",");
+    }
+
+    if (galleryArray) {
+      galleryArray.map((gallery) => {
+        deleteImage(gallery, imgPath, fs, path);
+      });
+    } else if (!galleryArray && galleryString && galleryString != "null") {
+      deleteImage(galleryString, imgPath, fs, path);
+    }
+    const result = await deleteSuivi.destroy();
+
+    if (!result) return res.json({ success: false });
+
+    if (productId) {
+      const allSuivis = await getSuivisByProduct(
+        suivis,
+        users,
+        res,
+        productId,
+        problems
+      );
+      res.json({ success: true, suivis: allSuivis });
+    } else {
+      const filterSuivis = await getSuivis(
+        suivis,
+        users,
+        products,
+        productTypes
+      );
+      res.json({ success: true, suivis: filterSuivis });
+    }
+  } catch (error) {
+    res.json({ success: false });
+    console.log("ERROR deleteSuivi", error);
   }
 };
 
-const uploadImageSuivi = async (req, res) => {
-  const { id, productId } = await req.body;
+const uploadImageSuivi = async (req, res, addedSuivi, productId) => {
+  try {
+    let gallery;
 
-  let gallery = "null";
-  let response;
+    if (req?.files?.length) {
+      gallery = await createImage(req, imgPath, sharp, path, fs);
+    }
+    let observation = addedSuivi.dataValues.observation;
+    addedSuivi.observation = `${observation}${gallery ? gallery : ""}`;
+    const result = await addedSuivi.save();
 
-  if (!id) return res.json({ success: false });
-
-  const addedSuivi = await suivis.findOne({
-    where: {
-      id: id,
-    },
-  });
-
-  if (!id) return res.json({ success: false });
-
-  if (req?.files?.length) {
-    const galleryArray = new Array();
-    response = req.files.map(async (file) => {
-      if (file.mimetype.split("/")[0] == "image") {
-        let date = new Date();
-        let filename = `${file.originalname.split(".")[0]}-${date.getDate()}-${
-          date.getMonth() + 1
-        }-${date.getFullYear()}-${date.getTime()}.webp`;
-        let webpData = await sharp(file.buffer).webp().toBuffer();
-        let webpFilePath = path.join(imgPath, filename);
-        fs.writeFileSync(webpFilePath, webpData);
-        galleryArray.push(`${process.env.SERVER_PATH}/img/${filename}`);
-      }
-    });
-    await Promise.all(response);
-    gallery = galleryArray.join(",");
+    if (!result) return res.json({ success: false });
+    const allSuivis = await getSuivisByProduct(
+      suivis,
+      users,
+      res,
+      productId,
+      problems
+    );
+    res.json({ success: true, suivis: allSuivis });
+  } catch (error) {
+    res.json({ success: false });
+    console.log("uploadImageSuivi", error);
   }
-
-  let observation = addedSuivi.dataValues.observation;
-
-  addedSuivi.observation = `${observation}${gallery}`;
-
-  const result = await addedSuivi.save();
-
-  if (!result) return res.json({ success: false });
-
-  const allSuivis = await suivis.findAll({
-    where: {
-      productId: productId,
-    },
-    include: [{ model: users, attribute: ["id", "name"] }],
-  });
-
-  res.json({ success: true, suivis: allSuivis });
 };
 
 const updateSuivi = async (req, res) => {
@@ -232,10 +192,10 @@ const updateSuivi = async (req, res) => {
   try {
     if (!id || !productId || !problem || !solution)
       return res.json({ success: false });
-
     const updatedSuivi = await suivis.findOne({ where: { id: id } });
-    let updatedObservation = `${observation};${
-      updatedSuivi.dataValues.observation.split(";")[1]
+    const gallery = updatedSuivi?.dataValues?.observation?.split(";")[1];
+    const updatedObservation = `${observation ? observation : ""};${
+      gallery ? gallery : ""
     }`;
     updatedSuivi.set({
       problem,
@@ -243,123 +203,87 @@ const updateSuivi = async (req, res) => {
       observation: updatedObservation,
     });
     const result = await updatedSuivi.save();
+
     if (!result) return res.json({ success: false });
-
-    const allSuivis = await suivis.findAll({
-      where: {
-        productId: productId,
-      },
-      include: [{ model: users, attribute: ["id", "name"] }],
-    });
-
-    res.json({ success: true, suivis: allSuivis });
+    await updateUpload(req, res, productId, updatedSuivi);
   } catch (error) {
     res.json({ success: false });
-    console.log(error);
+    console.log("ERROR updateSuivi", error);
   }
 };
 
-const updateUpload = async (req, res) => {
-  const { id, productId } = await req.body;
+const updateUpload = async (req, res, productId, updatedUpload) => {
   let updateGalleryArray;
   let newGallery;
   let observation;
   try {
-    if (!id || !productId) return res.json({ success: false });
-
-    const updatedUpload = await suivis.findOne({
-      where: {
-        id: id,
-      },
-    });
-
-    if (!updatedUpload) return res.json({ success: false });
-
-    observation = updatedUpload.dataValues.observation?.split(";")[0];
+    observation = updatedUpload?.dataValues?.observation?.split(";")[0];
 
     if (req?.files?.length) {
-      const newGalleryArray = new Array();
-      const response = req.files.map(async (file) => {
-        if (file.mimetype.split("/")[0] == "image") {
-          let date = new Date();
-          let filename = `${
-            file.originalname.split(".")[0]
-          }-${date.getDate()}-${
-            date.getMonth() + 1
-          }-${date.getFullYear()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.webp`;
-          let webpData = await sharp(file.buffer).webp().toBuffer();
-          let webpFilePath = path.join(imgPath, filename);
-          fs.writeFileSync(webpFilePath, webpData);
-          newGalleryArray.push(`${process.env.SERVER_PATH}/img/${filename}`);
-        }
-      });
-      await Promise.all(response);
-      newGallery = newGalleryArray.join(",");
+      newGallery = await createImage(req, imgPath, sharp, path, fs);
     }
-
     const updateGalleryString =
-      updatedUpload.dataValues.observation.split(";")[1];
+      updatedUpload?.dataValues?.observation?.split(";")[1];
 
-    if (updateGalleryString && updateGalleryString != "null") {
-      updateGalleryArray = updateGalleryString.split(",");
+    if (updateGalleryString && updateGalleryString != "") {
+      updateGalleryArray = updateGalleryString?.split(",");
     }
 
     if (updateGalleryArray) {
       updateGalleryArray.map((gallery) => {
-        let name = gallery.split("/")[gallery.split("/").length - 1];
-        let pathFile = path.join(imgPath, name);
-        fs.unlinkSync(pathFile, (err) => {
-          if (err) {
-            console.log(err);
-          }
-        });
+        deleteImage(gallery, imgPath, fs, path);
       });
     } else if (
       !updateGalleryArray &&
       updateGalleryString &&
-      updateGalleryString != "null"
+      updateGalleryString != ""
     ) {
-      let name =
-        updateGalleryString.split("/")[
-          updateGalleryString.split("/").length - 1
-        ];
-      let pathFile = path.join(imgPath, name);
-      fs.unlinkSync(pathFile, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
+      deleteImage(updateGalleryArray, imgPath, fs, path);
     }
-
-    updatedUpload.observation = `${observation};${newGallery}`;
-
+    updatedUpload.observation = `${observation ? observation : ""};${
+      newGallery ? newGallery : ""
+    }`;
     const result = await updatedUpload.save();
 
     if (!result) return res.json({ success: false });
-
-    const allSuivis = await suivis.findAll({
-      where: {
-        productId: productId,
-      },
-      include: [{ model: users, attribute: ["id", "name"] }],
-    });
-
-    if (!allSuivis) return res.json({ success: false });
-
+    const allSuivis = await getSuivisByProduct(
+      suivis,
+      users,
+      res,
+      productId,
+      problems
+    );
     res.json({ success: true, suivis: allSuivis });
   } catch (error) {
-    console.log(error);
     res.json({ success: false });
+    console.log("ERROR updateUpload", error);
   }
 };
 
 const getAllSuivis = async (req, res) => {
   try {
-    const filterSuivis = await getSuivis(suivis, users, products, productTypes);
-
-    res.json({ success: true, suivis: filterSuivis });
+    let year;
+    const filterSuivis = await getSuivis(
+      suivis,
+      users,
+      products,
+      productTypes,
+      res,
+      problems
+    );
+    let years = [];
+    const resultYear = filterSuivis.map((item) => {
+      const itemYear = item.createdAt.split("-")[0];
+      if (itemYear != year) {
+        year = itemYear;
+        years.push(itemYear);
+      }
+    });
+    await Promise.all(resultYear);
+    res.json({ success: true, suivis: filterSuivis, years: years });
   } catch (error) {
-    console.log(error);
+    res.json({ success: false });
+    console.log("ERROR getAllSuivis", error);
   }
 };
 
