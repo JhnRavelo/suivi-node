bb7950ca0bdea6e5031a974859d280fe03aa770e
JhnRavelo <johnravelo135@gmail.com>
2024-03-13 10:56:03 +0300

    add getStat route to suiviRouter

--- routers/suivisRouter.js
+++ routers/suivisRouter.js
@@ -6,6 +6,7 @@ const {
   deleteSuivi,
   updateSuivi,
   getAllSuivis,
+  getStatPerYear,
 } = require("../controllers/suivisController");
 const router = express.Router();
 const multer = require("multer");
@@ -18,5 +19,11 @@ router.post("/", verifyJWT, memoryStokage.any(), addSuivi);
 router.post("/delete", verifyJWT, deleteSuivi);
 router.put("/", verifyJWT, memoryStokage.any(), updateSuivi);
 router.get("/getAll", verifyJWT, verifyRole(process.env.PRIME), getAllSuivis);
+router.get(
+  "/getStat",
+  verifyJWT,
+  verifyRole(process.env.PRIME),
+  getStatPerYear
+);
 
 module.exports = router;
