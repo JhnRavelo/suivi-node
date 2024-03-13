accb2704ea76f54cf07da81c52d553e357a3cf38
JhnRavelo <johnravelo135@gmail.com>
2024-03-13 10:57:21 +0300

    add problems models to function getSuivis

--- utils/getSuivis.js
+++ utils/getSuivis.js
@@ -1,30 +1,40 @@
-const getSuivis = async (suivis, users, products, productTypes) => {
-    const allSuivis = await suivis.findAll({
-        include: [
-          { model: users },
-          { model: products, include: [{ model: productTypes }] },
-        ],
-      });
-  
-      if (!allSuivis) return res.json({ success: false });
-  
-      const filterSuivis = allSuivis.map((item) => {
-        let value = item.dataValues;
-  
-        return {
-          tech: value.user.name,
-          id: value.id,
-          type: value.product.productType.name,
-          problem: value.problem,
-          solution: value.solution,
-          observation: value.observation,
-          chantier: value.product.chantier,
-          client: value.product.client,
-          devis: value.product.devis,
-          createdAt: value.createdAt
-        };
-      });
-      return filterSuivis
-}
+const getProblem = require("./getProblem");
 
-module.exports = getSuivis
+const getSuivis = async (
+  suivis,
+  users,
+  products,
+  productTypes,
+  res,
+  problems
+) => {
+  const allSuivis = await suivis.findAll({
+    include: [
+      { model: users },
+      { model: products, include: [{ model: productTypes }] },
+      { model: problems, as: "problems" },
+    ],
+  });
+
+  if (!allSuivis) return res.json({ success: false });
+
+  const filterSuivis = allSuivis.map((item) => {
+    const value = item.dataValues;
+    const problem = getProblem(value);
+    return {
+      tech: value.user.name,
+      id: value.id,
+      type: value.product.productType.name,
+      problem: problem,
+      solution: value.solution,
+      observation: value.observation,
+      chantier: value.product.chantier,
+      client: value.product.client,
+      devis: value.product.devis,
+      createdAt: value.createdAt,
+    };
+  });
+  return filterSuivis;
+};
+
+module.exports = getSuivis;
