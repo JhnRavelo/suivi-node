221ae1a1fc71505810c0e7ec4e615ac6a9ea4cb9
JhnRavelo <johnravelo135@gmail.com>
2024-03-13 10:43:06 +0300

    create function getSuivisByProduct

---
+++ utils/getSuivisByProduct.js
@@ -0,0 +1,31 @@
+const getProblem = require("./getProblem");
+
+const getSuivisByProduct = async (suivis, users, res, productId, problems) => {
+  const allSuivis = await suivis.findAll({
+    where: {
+      productId: productId,
+    },
+    include: [
+      { model: users, attribute: ["id", "name"] },
+      { model: problems, as: "problems" },
+    ],
+  });
+
+  if (!allSuivis) return res.json({ success: false });
+  const filterSuivis = allSuivis.map((item) => {
+    const value = item.dataValues;
+    const problem = getProblem(value);
+    return {
+      id: value.id,
+      problem: problem,
+      solution: value.solution,
+      observation: value.observation,
+      user: `${value?.user?.name ? value?.user?.name : ""}`,
+      createdAt: value.createdAt.split(" ")[0],
+    };
+  });
+
+  return filterSuivis;
+};
+
+module.exports = getSuivisByProduct;
