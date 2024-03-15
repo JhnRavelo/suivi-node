8d1a802d1655b9e5639c64ad8a7bdbd8f6bdbcde
JhnRavelo <johnravelo135@gmail.com>
2024-03-15 17:01:17 +0300

    change getLogs in logController

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,6 +6,8 @@ const {
   users,
   problems,
 } = require("../database/models");
+const sequelize = require("sequelize");
+const cc = require("node-console-colors");
 const getProblem = require("../utils/getProblem");
 
 const getLogs = async (req, res) => {
@@ -30,6 +32,11 @@ const getLogs = async (req, res) => {
           ],
         },
       ],
+      attributes: [
+        [sequelize.literal("YEAR(logs.createdAt)"), "year"],
+        "createdAt",
+      ],
+      order: [["createdAt", "DESC"]],
     });
 
     if (!allLogs) return res.json({ success: false });
@@ -50,15 +57,15 @@ const getLogs = async (req, res) => {
           )}`
         );
       }
-      logsAll.push(
-        `${value.suivi.user.name} a effectué un suivi du produit ${
-          value.suivi.product.productType.name
-        } du devis ${value.suivi.product.devis};${value.createdAt.slice(
-          0,
-          16
-        )};${problem};${value.suivi.solution}`
-      );
+      logsAll.push({
+        log: `${value.suivi.user.name} a effectué un suivi du produit ${value.suivi.product.productType.name} du devis ${value.suivi.product.devis} associé au client ${value.suivi.product.client}`,
+        year: value.year,
+        createdAt: value.createdAt,
+        problem: problem,
+        solution: value.suivi.solution,
+      });
     });
+    console.log(cc.set("fg_blue", "LOG"), logsAll);
     res.json({ success: true, logs: filterLogs, allLogs: logsAll });
   } catch (error) {
     res.json({ success: false });
