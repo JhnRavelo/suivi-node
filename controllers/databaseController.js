c82c915683a220c9f38401be201e1660e9a3fadb
JhnRavelo <johnravelo135@gmail.com>
2024-03-22 09:00:21 +0300

    add database import

--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -1,33 +1,37 @@
 const db = require("../database/models");
-const sqEI = require("sequelize-import-export");
+const sqEI = require("../database/sequelize-import-export");
 const path = require("path");
 const fs = require("fs");
 const generateRandomText = require("../utils/generateRandomText");
+const createDirectory = require("../utils/createDirectory");
 
 const exportPath = path.join(__dirname, "..", "database", "export");
+const importPath = path.join(__dirname, "..", "database", "import");
 
 const exportDatabase = async (req, res) => {
   try {
-    fs.readdir(exportPath, (err, files) => {
-      if (err) return console.log(err);
-      const fileName = files.find((item) => item.includes(".tmp"));
-      if (fileName) {
-        fs.unlinkSync(path.join(exportPath, fileName));
-      }
-    });
-    const users = await db.users.findAll();
+    const date = new Date();
     const dbex = new sqEI(db);
+    const users = await db.users.findAll({
+      where: {
+        role: process.env.PRIME2,
+      },
+    });
     const dataStringInFile = db.users.prototype.generateData(users);
-    const fileName = generateRandomText(10);
-    fs.writeFileSync(
-      path.join(exportPath, `${fileName}.tmp`),
-      dataStringInFile,
-      {
-        encoding: "utf8",
-        flag: "w",
-      }
-    );
-    const pathExportFile = path.join(exportPath, "export.sequelize");
+    const fileName =
+      generateRandomText(10) +
+      `-${
+        date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds()
+      }.tmp`;
+    const exportDir = createDirectory(exportPath, 0);
+    fs.writeFileSync(path.join(exportDir, fileName), dataStringInFile, {
+      encoding: "utf8",
+      flag: "w",
+    });
+    const exportFileName = `export-${
+      date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds()
+    }.sequelize`;
+    const pathExportFile = path.join(exportDir, exportFileName);
     dbex
       .export(pathExportFile, { excludes: ["users"] })
       .then((path) => {
@@ -41,4 +45,44 @@ const exportDatabase = async (req, res) => {
   }
 };
 
-module.exports = { exportDatabase };
+const importDatabase = async (req, res) => {
+  try {
+    const date = new Date();
+    const dbex = new sqEI([
+      db.productTypes,
+      db.products,
+      db.problems,
+      db.suivis,
+      db.logs,
+    ]);
+    console.log("FILE", req.files[0]);
+    if (!req.files && !req.files[0])
+      return res.json({ success: false, message: "Aucun fichier envoyer" });
+    if (!req.files[0].originalname.includes(".sequelize"))
+      return res.json({
+        success: false,
+        message: "Le fichier doit être de type sequelize",
+      });
+    const fileBuffer = req.files[0].buffer;
+    const importDir = createDirectory(importPath, 0);
+    const importFileName = `import-${
+      date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds()
+    }.sequelize`;
+    const importFilePath = path.join(importDir, importFileName);
+    fs.writeFileSync(importFilePath, fileBuffer, {
+      encoding: "utf8",
+      flag: "w",
+    });
+    dbex
+      .import(importFilePath, { overwrite: true, excludes: ["users"] })
+      .then((file) => {
+        console.log(file);
+      })
+      .catch((err) => console.log("ERROR IMPORT SEQUELIZE", err));
+    res.json({ success: true });
+  } catch (error) {
+    console.log("ERROR IMPORT DATABASE", error);
+  }
+};
+
+module.exports = { exportDatabase, importDatabase };
--- routers/databaseRouter.js
+++ routers/databaseRouter.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const verifyJWT = require("../middlewares/verifyJWT")
 const verifyRole = require("../middlewares/verifyRole")
-const { exportDatabase } = require("../controllers/databaseController")
+const { exportDatabase, importDatabase } = require("../controllers/databaseController")
 const router = express.Router()
+const multer = require("multer")
+
+const memoryStokage = multer({ storage: multer.memoryStorage() });
 
 router.get("/export", verifyJWT, verifyRole(process.env.PRIME), exportDatabase)
+router.post("/import", verifyJWT, verifyRole(process.env.PRIME), memoryStokage.any(), importDatabase)
 
 module.exports = router
