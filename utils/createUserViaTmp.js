d6be4bafa3601b2aa9736674c21bcb3c38bc20af
JhnRavelo <johnravelo135@gmail.com>
2024-03-25 15:15:42 +0300

    create createUserViaTmp utils

---
+++ utils/createUserViaTmp.js
@@ -0,0 +1,27 @@
+const verifyUserAndCreate = async (row, db) => {
+  console.log("ID", row.id);
+  const existUser = await db.users.findOne({ where: { id: row.id } });
+  if (existUser) {
+    existUser.set(row);
+    await existUser.save();
+  } else await db.users.create(row);
+};
+
+const createUserViaTmp = async (fs, path, jwt, db) => {
+  const readTmp = fs.readFileSync(path, {
+    encoding: "utf8",
+  });
+  jwt.verify(readTmp, process.env.DATA_TOKEN, async (err, decoded) => {
+    if (err) return console.log("ERROR VERIFY IN APP", err);
+    const rows = JSON.parse(decoded.data);
+    if (rows?.length) {
+      rows.map(async (row) => {
+        await verifyUserAndCreate(row, db);
+      });
+    } else {
+      await verifyUserAndCreate(rows, db);
+    }
+  });
+};
+
+module.exports = createUserViaTmp;
