89738236ab61e3ab05c4f12db3caef59039b5286
JhnRavelo <johnravelo135@gmail.com>
2024-03-25 15:20:06 +0300

    reformat database router

--- routers/databaseRouter.js
+++ routers/databaseRouter.js
@@ -1,14 +1,36 @@
-const express = require("express")
-const verifyJWT = require("../middlewares/verifyJWT")
-const verifyRole = require("../middlewares/verifyRole")
-const { exportDatabase, importDatabase, readExport } = require("../controllers/databaseController")
-const router = express.Router()
-const multer = require("multer")
+const express = require("express");
+const verifyJWT = require("../middlewares/verifyJWT");
+const verifyRole = require("../middlewares/verifyRole");
+const {
+  exportDatabase,
+  importDatabase,
+  readExport,
+  restoreExport,
+} = require("../controllers/databaseController");
+const router = express.Router();
+const multer = require("multer");
 
 const memoryStokage = multer({ storage: multer.memoryStorage() });
 
-router.get("/export", verifyJWT, verifyRole(process.env.PRIME), exportDatabase)
-router.get("/read/export", verifyJWT, verifyRole(process.env.PRIME), readExport)
-router.post("/import", verifyJWT, verifyRole(process.env.PRIME), memoryStokage.any(), importDatabase)
+router.get("/export", verifyJWT, verifyRole(process.env.PRIME), exportDatabase);
+router.get(
+  "/read/export",
+  verifyJWT,
+  verifyRole(process.env.PRIME),
+  readExport
+);
+router.post(
+  "/import",
+  verifyJWT,
+  verifyRole(process.env.PRIME),
+  memoryStokage.any(),
+  importDatabase
+);
+router.post(
+  "/restore/export",
+  verifyJWT,
+  verifyRole(process.env.PRIME),
+  restoreExport
+);
 
-module.exports = router
+module.exports = router;
