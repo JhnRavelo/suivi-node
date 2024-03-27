e9859bc9964e16339b371b680058f480ef482a6d
JhnRavelo <johnravelo135@gmail.com>
2024-03-27 10:14:40 +0300

    create compressZip utils

---
+++ utils/compressZip.js
@@ -0,0 +1,38 @@
+const archiver = require("archiver");
+
+const compressZip = async (zipFile, fs, path, dirPath, res, filePath, date) => {
+  const output = fs.createWriteStream(path.join(dirPath, `export${date}zip`));
+  const archive = archiver("zip", {
+    zlib: { level: 9 },
+  });
+  output.on("close", function () {
+    console.log(archive.pointer() + " total bytes");
+    res.download(output.path);
+    fs.unlinkSync(filePath);
+  });
+
+  // @see: https://nodejs.org/api/stream.html#stream_event_end
+  output.on("end", function () {
+    console.log("Data has been drained");
+  });
+
+  archive.on("warning", function (err) {
+    if (err.code === "ENOENT") {
+      console.log("WARNING ENOENT");
+    } else {
+      throw err;
+    }
+  });
+
+  archive.on("error", function (err) {
+    res.json({ success: false, message: "Erreur compression" });
+    throw err;
+  });
+
+  archive.pipe(output);
+  archive.file(path.join(dirPath, zipFile), { name: zipFile });
+  archive.directory(path.join(__dirname, "..", "public"), false);
+  archive.finalize();
+};
+
+module.exports = compressZip;
